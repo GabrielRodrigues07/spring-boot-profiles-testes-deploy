d557423dfdefb2db54ee01ed1ab0578310f04b85
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-02-03 10:49:23 -0300

    Testando o repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,25 +1,42 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.model.Curso;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CursoRepositoryTest {
 
-    @Autowired
+    @Mock
     private CursoRepository repository;
 
+    @InjectMocks
+    private Curso curso;
+
+    @Before("")
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
     void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        curso.setNome("HTML 5");
+        curso.setCategoria("Programação");
+
         String nomeCurso = "HTML 5";
+        Mockito.when(repository.findByNome(nomeCurso)).thenReturn(curso);
+
         Curso curso = repository.findByNome(nomeCurso);
+
         Assertions.assertNotNull(curso);
         Assertions.assertEquals(nomeCurso, curso.getNome());
     }
